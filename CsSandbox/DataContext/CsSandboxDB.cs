9574c2dd10e0cec93cebb3d7ba3573579cabe38f
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2014-12-16 16:11:17 +0500

    add database initialization strategy

--- CsSandbox/DataContext/CsSandboxDB.cs
+++ CsSandbox/DataContext/CsSandboxDB.cs
@@ -1,10 +1,17 @@
 ﻿using System.Data.Entity;
+using CsSandbox.Migrations;
 using CsSandbox.Models;
 
 namespace CsSandbox.DataContext
 {
 	public class CsSandboxDb : DbContext
 	{
+		public CsSandboxDb()
+			: base("DefaultConnection")
+		{
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<CsSandboxDb, Configuration>());
+		}
+
 		public DbSet<SubmissionDetails> Submission { get; set; }
 		public DbSet<User> Users { get; set; }
 	}
