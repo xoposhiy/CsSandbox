48c3b969b3155140f46ff35b7f42db7e29d59c5b
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2015-01-12 14:07:10 +0500

    StringBuilder Capacity in LimitedStringWriter

--- CsSandboxer/LimitedStringWriter.cs
+++ CsSandboxer/LimitedStringWriter.cs
@@ -9,27 +9,31 @@ namespace CsSandboxer
 		private readonly int _maxLength;
 		private const int DefaultLength = 10 * 1024 * 1024;
 
-		public bool HasOutputLimit { get; private set; }
+		private bool HasOutputLimit { get; set; }
 
 		public LimitedStringWriter(int maxLength = DefaultLength)
 		{
 			_maxLength = maxLength;
+			base.GetStringBuilder().Capacity = maxLength;
 		}
 
 		public LimitedStringWriter(IFormatProvider formatProvider, int maxLength = DefaultLength) : base(formatProvider)
 		{
 			_maxLength = maxLength;
+			base.GetStringBuilder().Capacity = maxLength;
 		}
 
 		public LimitedStringWriter(StringBuilder sb, int maxLength = DefaultLength) : base(sb)
 		{
 			_maxLength = maxLength;
+			base.GetStringBuilder().Capacity = maxLength;
 		}
 
 		public LimitedStringWriter(StringBuilder sb, IFormatProvider formatProvider, int maxLength = DefaultLength)
 			: base(sb, formatProvider)
 		{
 			_maxLength = maxLength;
+			base.GetStringBuilder().Capacity = maxLength;
 		}
 
 		public override void Write(char value)
